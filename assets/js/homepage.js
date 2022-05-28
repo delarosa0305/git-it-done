2c36f75544b5a180bb6f78f225f805b4a6c95043
delarosa0305 <delarosa2381i@gmail.com>
2022-05-27 21:13:45 -0400

    add programming language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var getUserRepos = function (user) {
     // format the github api url
@@ -26,6 +27,20 @@ var getUserRepos = function (user) {
 };
 getUserRepos();
 
+var getFeaturedRepos = function (language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+};
+
 var formSubmitHandler = function (event) {
     event.preventDefault();
     console.log(event);
@@ -35,13 +50,24 @@ var formSubmitHandler = function (event) {
 
     if (username) {
         getUserRepos(username);
-        
+
         repoContainerEl.textContent = "";
         nameInputEl.value = "";
     } else {
         alert("Please enter a GitHub username");
     }
 };
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+}
 
 var displayRepos = function (repos, searchTerm) {
     // check if api returened any repos
@@ -90,3 +116,4 @@ var displayRepos = function (repos, searchTerm) {
 }
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
